cb123eefb999700aed9bda5c9c9ea924ff5da9d8
Vitor Lacerda Faria <vitorlacerdafaria7@gmail.com>
2023-05-12 14:03:39 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use rusty_audio::Audio;
+use std::{error::Error, io, time::Duration};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explosion", "src/sounds/explosion.wav");
+    audio.add("lose", "src/sounds/lose.wav");
+    audio.add("music", "src/sounds/music.mp3");
+    audio.add("shot", "src/sounds/shot.wav");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Game loop
+    'gameloop: loop {
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    // Cleanup
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
+}
